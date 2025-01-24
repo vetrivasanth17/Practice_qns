6feb6f5b48f288de592147551074d8e241c8c71e
VETRI VASANTH M <137294887+vetrivasanth17@users.noreply.github.com>
2025-01-24 17:09:52 +0530

    Add files via upload

---
+++ LeapYear.java
@@ -0,0 +1,20 @@
+package vetriEclipse;
+import java.util.Scanner;
+
+public class LeapYear {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter Year: ");
+		int year=sc.nextInt();
+		if((year%4==0 && year%100!=0) ||(year%400==0)) {
+			System.out.println("It is leap year");
+		}
+		else {
+			System.out.println("It is not a leap year");
+			
+		}
+		
+		
+	}
+
+}
