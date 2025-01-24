ead8beb07b082aa8e55d2a3b103198805a682d33
VETRI VASANTH M <137294887+vetrivasanth17@users.noreply.github.com>
2025-01-24 20:34:37 +0530

    Update LeapYear.java

--- LeapYear.java
+++ LeapYear.java
@@ -1,4 +1,3 @@
-package vetriEclipse;
 import java.util.Scanner;
 
 public class LeapYear {
@@ -6,9 +5,12 @@ public class LeapYear {
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter Year: ");
 		int year=sc.nextInt();
+		/*If the year leaves a remainder of zero when divided by 4 and 400
+			and does not leave a remainder of zero when divided by 100*/
 		if((year%4==0 && year%100!=0) ||(year%400==0)) {
 			System.out.println("It is leap year");
 		}
+		//otherwise it is not a leap year
 		else {
 			System.out.println("It is not a leap year");
 			
