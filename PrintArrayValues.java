d291e3ac1b677b94e294cc0f667c41277ba532b1
VETRI VASANTH M <137294887+vetrivasanth17@users.noreply.github.com>
2025-01-30 21:42:51 +0530

    Add files via upload

---
+++ PrintArrayValues.java
@@ -0,0 +1,13 @@
+public class PrintArrayValues{
+    public static void main(String[] args) {
+        int[] arr={1,2,1,4,2};
+
+
+        //iterating through each values in an array and printing it using for loop
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+}
+
