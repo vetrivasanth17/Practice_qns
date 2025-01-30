e341cb43b1a7379d75e38c0e95bc40db65ed66fd
VETRI VASANTH M <137294887+vetrivasanth17@users.noreply.github.com>
2025-01-30 23:33:26 +0530

    Add files via upload

---
+++ InfiniteWhileLoop.java
@@ -0,0 +1,8 @@
+public class InfiniteWhileLoop {
+    public static void main(String[] args) {
+        while(true){
+            System.out.println("Endless loop using while loop!!");
+        }
+    }
+    
+}
