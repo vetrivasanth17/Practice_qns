49c51146cec977ff53ffc2fd373b478ae6e8a26f
VETRI VASANTH M <137294887+vetrivasanth17@users.noreply.github.com>
2025-02-04 21:02:47 +0530

    Add files via upload

---
+++ ByteInfo.java
@@ -0,0 +1,15 @@
+public class ByteInfo {
+    public static void main(String[] args) {
+
+        //Prints Size of the Byte
+        System.out.println(Byte.SIZE);
+
+        //Prints the Minimum value of Byte
+        System.out.println(Byte.MIN_VALUE);
+
+        //Prints Maximum Value of Byte
+        System.out.println(Byte.MAX_VALUE);
+    }
+    
+}
+
